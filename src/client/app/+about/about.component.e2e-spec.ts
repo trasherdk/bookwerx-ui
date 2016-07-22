89907bc4ac2779e6eb08d93c28cb7b8b2daa3097
radloff <bostontrader@gmail.com>
2016-07-23 03:37:15 +0800

    Minor edits.

--- src/client/app/+about/about.component.e2e-spec.ts
+++ src/client/app/+about/about.component.e2e-spec.ts
@@ -1,11 +1,11 @@
 describe('About', () => {
 
   beforeEach( () => {
-    browser.get('/about');
-  });
+    browser.get('/about')
+  })
 
   it('should have correct feature heading', () => {
-    expect(element(by.css('sd-about h2')).getText()).toEqual('Features');
-  });
+    expect(element(by.css('sd-about h2')).getText()).toEqual('Features')
+  })
 
-});
+})
